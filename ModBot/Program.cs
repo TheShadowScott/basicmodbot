276eed3b86dcd8fa75e5be57a68a194f5eff0762
Scott Pendland <smpendland@outlook.com>
2023-02-19 06:36:43 -0800

    Add project files.

---
+++ ModBot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static ModBot.DatabaseCommands;
+using DSharpPlus;
+using DSharpPlus.SlashCommands;
+
+namespace ModBot;
+
+class Program
+{
+    static async Task Main()
+    {
+        DotNetEnv.Env.TraversePath().Load();
+
+        var config = new DiscordConfiguration()
+        {
+            Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
+            TokenType = TokenType.Bot,
+            AutoReconnect = true,
+            MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+            LogTimestampFormat = "dd MMM yyyy - HH:mm:ss.fff zzz"
+        };
+
+        var client = new DiscordClient(config);
+
+        var slash = client.UseSlashCommands();
+        // slash.RegisterCommands(typeof(NullCommands), 772938218471292958);
+
+        slash.RegisterCommands(typeof(DiscordCommands));
+
+        await client.ConnectAsync();
+
+        foreach (var guild in client.Guilds) Console.WriteLine(guild);
+
+        Thread.Sleep(Timeout.Infinite);
+
+    }
+}
---
+++ ModBot/RoleCommands.cs
@@ -0,0 +1,133 @@
+﻿using DSharpPlus;
+using static System.Environment;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.Entities;
+using System.Diagnostics;
+using System.Globalization;
+using static ModBot.DatabaseCommands;
+using DiscordBot;
+using Microsoft.Win32.SafeHandles;
+using System.Security.Cryptography.X509Certificates;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Diagnostics.Metrics;
+
+namespace ModBot;
+public sealed partial class DiscordCommands : ApplicationCommandModule
+{
+    public enum UrgencyLevel
+    {
+        Low = 1,
+        Normal = 2,
+        High = 3,
+        Urgent = 4
+    }
+    [SlashCommand("modmail", "Sends a message to the server moderators.")]
+    public static async Task ModMail(InteractionContext ctx, [Option("urgency", "Urgency of message")] UrgencyLevel urgency,
+        [Option("message", "Message to send to the moderators")] string message)
+    {
+        var channel = await ctx.Client.GetChannelAsync((ulong)Convert.ToInt64(GetEnvironmentVariable("MODMAIL_CHANNEL_ID")));
+
+        var embed = new DiscordEmbedBuilder()
+            .WithAuthor('@' + ctx.User.Username)
+            .WithTimestamp(DateTimeOffset.UtcNow)
+            .WithDescription(message)
+            .WithColor(
+                (int)urgency switch
+                {
+                    4 => 0x72286f,
+                    3 => 0xb80f0a,
+                    2 => 0x933b17,
+                    1 => 0xffdf00,
+                    _ => 0xffffff
+                }
+            );
+
+        var msg = new DiscordMessageBuilder()
+            .WithEmbed(embed);
+        if ((int)urgency >= Convert.ToInt32(GetEnvironmentVariable("URGENCY_PING_LEVEL")))
+            msg.WithContent("<@&814129218153086996>");
+
+        await channel.SendMessageAsync(msg);
+        await ctx.CreateResponseAsync("Message sent!");
+    }
+    [SlashCommand("addrole", "Adds a role to a user.")]
+    public static async Task AddRole(InteractionContext ctx, [Option("user", "User to give the role to")] DiscordUser user,
+        [Option("role", "Role to give the user")] DiscordRole role)
+    {
+        if (!ctx.Member.Permissions.HasPermission(Permissions.ManageRoles) || ctx.Channel.IsPrivate)
+        {
+            await ctx.CreateResponseAsync("You need the `Manage Roles` permission to use this command.");
+            return;
+        }
+
+        if (((DiscordMember)user).Roles.Contains(role))
+        {
+            await ctx.CreateResponseAsync("This user already has this role!");
+            return;
+        }
+
+        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+        await ((DiscordMember)user).GrantRoleAsync(role);
+
+        await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"Successfully gave <@{user.Id}> the <@&{role.Id}> role!"));
+    }
+    [SlashCommand("removerole", "Adds a role to a user.")]
+    public static async Task Remove(InteractionContext ctx, [Option("user", "User to give the role to")] DiscordUser user,
+        [Option("role", "Role to give the user")] DiscordRole role)
+    {
+        if (!ctx.Member.Permissions.HasPermission(Permissions.ManageRoles) || ctx.Channel.IsPrivate)
+        {
+            await ctx.CreateResponseAsync("You need the `Manage Roles` permission to use this command.");
+            return;
+        }
+
+        if (!(((DiscordMember)user).Roles.Contains(role)))
+        {
+            await ctx.CreateResponseAsync("This user doesn't have this role!");
+            return;
+        }
+
+        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+        try
+        {
+            await ((DiscordMember)user).GrantRoleAsync(role);
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"Successfully removed the <@&{role.Id}> role from <@{user.Id}>!"));
+        }
+        catch (Exception e)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Sorry I couldn't complete that operation."));
+            Console.WriteLine(e);
+        }
+    }
+    [SlashCommand("rename", "Sets the nickname of a member")]
+    public static async Task Nick(InteractionContext ctx, [Option("user", "User to rename.")] DiscordUser user, [Option("name", "Name to change to")] string name,
+        [Option("reason", "Reason for the name change")] string reason)
+    {
+        if (!ctx.Member.Permissions.HasPermission(Permissions.ManageNicknames) || ctx.Channel.IsPrivate)
+        {
+            await ctx.CreateResponseAsync("You need the `Manage Nicknames` permission");
+            return;
+        }
+        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+        try
+        {
+            await ((DiscordMember)user).ModifyAsync(x =>
+            {
+                x.Nickname = name;
+                x.AuditLogReason = reason;
+            });
+
+            await ExecCommand("NICK", user.Id, reason + $" (Name changed to {name} from {user.Username}.)", ctx.User);
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(
+                QuickCreate.QuickEmbed(@$"Changed the nickname for <@{user.Id}> ({user.Username}) to {name} for {reason}.", 0x555555)
+                ));
+        }
+        catch (Exception e)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Something went wrong."));
+        }
+    }
+}
