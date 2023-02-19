1cf6e9b55a398d89cdecb02f08bfc33d91da6e30
Scott Pendland <smpendland@outlook.com>
2023-02-19 12:28:50 -0800

    Refactoring | Added funcs

--- ModBot/DisciplineCommands.cs
+++ ModBot/DisciplineCommands.cs
@@ -35,63 +35,6 @@ public sealed partial class DiscordCommands : ApplicationCommandModule
         await (user as DiscordMember)!.SendMessageAsync(QuickCreate.QuickEmbed(@$"You have been warned in {ctx.Guild.Name} for {reason}", 0xFFEA00));
     }
 
-    [SlashCommand("fetch", "Fetches a user's discipline logs.")]
-    public async Task FetchUser(InteractionContext ctx, [Option("user", "User to access")] DiscordUser user)
-    {
-        try
-        {
-            if (!ctx.CheckPermissions(Permissions.ModerateMembers))
-            {
-                await ctx.CreateResponseAsync("You must have the `ManageRoles` permission to use this command", true);
-                return;
-            }
-
-            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-
-            var results = FetchData(user.Id);
-            results = results.OrderBy(i => i.Item6).ToList();
-            results.Reverse();
-            var splitResult = results.ChunkBy(6);
-
-            if (results.Count == 0)
-            {
-                await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("No Logs Found!"));
-                return;
-            }
-
-            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"Found Results for <@{user.Id}>:"));
-            int i = 0;
-
-            foreach (var item in splitResult)
-            {
-                var warnings = new DiscordEmbedBuilder();
-                try
-                {
-                    foreach ((string id, string type, long _, long perp, string reason, string timestamp) in item)
-                    {
-                        warnings.AddField(type, $"**Perpetrator**: <@{perp}> -  {perp}\n**Reason**: {reason}\n**Time**: <t:{((DateTimeOffset)DateTime.Parse(timestamp)).ToUnixTimeSeconds()}:F>\n**Id No**: {id}");
-                    }
-                    warnings.WithTitle($"Dicipline Logs for {item[0].Item3}")
-                        .WithColor(0x20afff)
-                        .WithTimestamp(DateTimeOffset.UtcNow)
-                        .WithFooter($"Page {++i}")
-                        .WithDescription("");
-
-                    await ctx.Channel.SendMessageAsync(warnings);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            await ctx.CreateResponseAsync("Something Went Wrong");
-        }
-    }
-
     [SlashCommand("kick", "Kicks a user")]
     public async Task KickUser(InteractionContext ctx, [Option("user", "User to kick")] DiscordUser user,
         [Option("reason", "Reason to kick")] string reason = "No Reason Provided")
@@ -137,37 +80,6 @@ public sealed partial class DiscordCommands : ApplicationCommandModule
         await user.UnbanAsync(ctx.Guild, reason);
         await ExecCommand("UNBAN", user.Id, reason, ctx.User);
         await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(QuickCreate.QuickEmbed(@$"<@{user.Id}> has been unbanned for {reason}.", 0x39845F)));
-    }
-    [SlashCommand("droplog", "Drops a discipline log.")]
-    public async Task DropData(InteractionContext ctx, [Option("log", "Id of the log to drop (should be exactly 36 characters).")] string id)
-    {
-        if (!ctx.CheckPermissions(Permissions.ModerateMembers))
-        {
-            await ctx.CreateResponseAsync("You must have the `ManageRoles` permission to use this command", true);
-            return;
-        }
-        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-        await Drop(id);
-        await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"Log with id `{id}` has been DROPPED and is no longer accessable."));
-    }
-    [SlashCommand("note", "Adds a note to a user.")]
-    public async Task Note(InteractionContext ctx, [Option("user", "User to issue the warning to.")] DiscordUser user,
-        [Option("note", "Note to add to the user.")] string note)
-    {
-        if (!ctx.CheckPermissions(Permissions.ModerateMembers))
-        {
-            await ctx.CreateResponseAsync("You must have the `ManageRoles` permission to use this command", true);
-            return;
-        }
-        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
-        var time = DateTime.UtcNow.ToString("O", CultureInfo.InvariantCulture);
-
-        await ExecCommand("NOTE", user.Id, note, ctx.User, time);
-
-        var embed = QuickCreate.QuickEmbed(@$"Added note to <@{user.Id}>: {note}", 0x49B6DF);
-
-        await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(embed));
-
     }
     [SlashCommand("timeout", "Time outs the specified user")]
     public async Task TimeOutUser(InteractionContext ctx, [Option("user", "User to timeout")] DiscordUser user,
